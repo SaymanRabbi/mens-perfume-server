542dc3aed8e4abdd8c275c30453c2e0e68ba5351
sayman <mdrabbi.asm@gmail.com>
2022-04-29 17:14:42 +0600

    monogodb connect

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const port = process.env.PORT||5000
-
+const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 //middleware;
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yx5wc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('productsCollection').collection('product');
+        app.post('/product', async (req, res) => {
+            const doc = {
+                title: "Record of a Shriveled Datum",
+                content: "No bytes, no problem. Just insert a document, in MongoDB",
+            }
+            const result = await productCollection.insertOne(doc);
+            console.log(result)
+            res.send(result)
+        })
+        
+    }
+    finally {
+        // await client.close()
+    }
+}
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
